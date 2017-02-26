522f996904ce58bc541025cc19b0da1beaa592d0
Tafarel Yan <tafarel.yan@gmail.com>
2017-02-26 18:25:11 -0300

    First upload to PyPi

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='cinemark-python',
+      version='0.0',
+      description='Wrapper for Cinemark Brasil API',
+      author='Tafarel Yan',
+      author_email='tafarel.yan@gmail.com',
+      url='https://github.com/ArrowsX/cinemark-python',
+      packages=['cinemark'],
+      )
