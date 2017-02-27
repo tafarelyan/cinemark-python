a36099a673a7ec4ba65d864024d4f0c2edcd81ca
Tafarel Yan <tafarel.yan@gmail.com>
2017-02-27 13:35:32 -0300

    Basic functionality

--- cinemark/api.py
+++ cinemark/api.py
@@ -1,3 +1,4 @@
+from datetime import datetime, date
 import xml.etree.ElementTree as ET
 
 import requests
@@ -9,14 +10,34 @@ class Cinemark(object):
         r = requests.get('http://www.cinemark.com.br/programacao.xml')
         self.root = ET.fromstring(r.content)
 
-        if kwargs.get('cinema'):
-            self.cinema = self._validar_cinema(kwargs.get('cinema'))
+        if kwargs.get('cidade'):
+            self.cidade = self._validar_cidade(kwargs.get('cidade'))
+
+        elif kwargs.get('cinema'):
+            self.cinema, self.cidade = self._validar_cinema(kwargs.get('cinema'))
 
     def _validar_cinema(self, cinema):
-        cinemas = {c[0].text: c.attrib['id'] for c in self.root[2]}
-        if isinstance(cinema, str):
-            if cinema in cinemas.keys():
-                return cinemas[cinema]
-        elif isinstance(cinema, int):
-            if str(cinema) in cinemas.values():
-                return cinema
+        for c in self.root.findall('./complexos//cinema'):
+            if c[0].text == cinema:
+                return c.attrib['id'], c[6].attrib['id']
+    
+    @staticmethod
+    def _clean(text):
+        return ' '.join(text.strip().split())
+
+    @property
+    def programacao(self):
+        cinema = self.root[3][0].find('cinema[@id="%s"]' % self.cinema)
+        for film in cinema.findall('filme'):
+            yield self._get_titulo(film.attrib['id'])
+
+    def _get_titulo(self, filme_id):
+        filme = self.root[3][1].find('filme[@id="%s"]' % filme_id)
+        return {
+            'titulo': filme[0].text,
+            'cartaz': filme[13].attrib['id'],
+            'genero': filme[3].text,
+            'duracao': filme[4].text,
+            'censura': filme[5].text,
+            'sinopse': self._clean(filme[6].text),
+        }
