0c49c53c4a6ad59d5ae4f8d00232b7ca156471ac
Tafarel Yan <tafarel.yan@gmail.com>
2017-02-28 09:10:47 -0300

    Updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='cinemark-python',
-      version='0.0',
+      version='0.0.1',
       description='Wrapper for Cinemark Brasil API',
       author='Tafarel Yan',
       author_email='tafarel.yan@gmail.com',
