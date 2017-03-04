bcefaac377d5729301952f967333085d75cbf0c3
Tafarel Yan <tafarel.yan@gmail.com>
2017-03-04 19:53:02 -0300

    Implement alternative cinemas to film & film by city

--- cinemark/api.py
+++ cinemark/api.py
@@ -4,10 +4,6 @@ import xml.etree.ElementTree as ET
 import requests
 
 
-class CinemarkError(Exception):
-    pass
-
-
 class Cinemark(object):
 
     def __init__(self, **kwargs):
@@ -15,21 +11,36 @@ class Cinemark(object):
         self.root = ET.fromstring(r.content)
 
         if kwargs.get('cinema'):
-            self.cinema = self._validar_cinema(kwargs.get('cinema'))
-        
+            self.cinema, self.cidade = self._validar_cinema(kwargs.get('cinema'))
+
+        elif kwargs.get('cidade'):
+            self.cidade = self._validar_cidade(kwargs.get('cidade'))
+
     def _validar_cinema(self, cinema):
         for c in self.root.findall('./complexos//cinema'):
             if c[0].text == cinema:
+                return c.get('id'), c[6].get('id')
+
+    def _validar_cidade(self, cidade):
+        for c in self.root.findall('./cidades//'):
+            if cidade == c.text:
                 return c.get('id')
-        else:
-            raise CinemarkError("Cinema não encontrado")
 
     @staticmethod
     def _clean(text):
         try:
             return ' '.join(text.strip().split())
-        except:
+        except AttributeError:
             return ''
+
+    @property
+    def cinemas_na_cidade(self):
+        for c in self.root.findall('./complexos//cinema'):
+            if self.cidade == c[6].get('id'):
+                yield {
+                    'id': c.get('id'),
+                    'nome': c[0].text,
+                }
 
     @property
     def programacao(self):
@@ -42,7 +53,7 @@ class Cinemark(object):
         return {
             'id': filme.get('id'),
             'titulo': filme[0].text,
-            'cartaz': '/' + filme[13].get('src'),
+            'cartaz_url': 'https://www.cinemark.com.br/' + filme[13].get('src'),
             'genero': filme[3].text,
             'duracao': filme[4].text,
             'censura': filme[5].text,
@@ -53,17 +64,33 @@ class Cinemark(object):
         cinema = self.root[3][0].find('cinema[@id="%s"]' % self.cinema)
         horarios = cinema.find('filme[@id="%s"]/horarios' % filme_id)
 
-        if not horarios:
-            raise CinemarkError("Não há horários disponíveis")
+        if horarios:
+            for horario in horarios:
+                if self._disponivel(horario.get('legenda'), data):
+                    yield {
+                        'horario': horario.text,
+                        'sala': horario.get('sala')
+                    }
 
-        for horario in horarios:
-            if self._disponivel(horario.get('legenda'), data):
-                yield {
-                    'horario': horario.text,
-                    'sala': horario.get('sala')
-                }
+        else:
+            print("Não há horários disponíveis em", self._cinema_by_id(self.cinema))
+            yield from self._achar_cinema(filme_id)
 
     def _disponivel(self, legenda_id, data):
         legenda = self.root[3][0][-1].find('legenda[@id="%s"]' % legenda_id)
         if data in (data.text for data in legenda.find('datas')):
             return True
+
+    def _achar_cinema(self, filme_id):
+        for cinema in self.root[3][0].findall('cinema[@cidade="%s"]' % self.cidade):
+            for filme in cinema.findall('filme'):
+                if str(filme_id) == filme.get('id'):
+                    yield {
+                        'id': cinema.get('id'),
+                        'nome': self._cinema_by_id(cinema.get('id')),
+                    }
+
+    def _cinema_by_id(self, cinema_id):
+        for c in self.root.findall('./complexos//cinema'):
+            if cinema_id == c.get('id'):
+                return c[0].text
